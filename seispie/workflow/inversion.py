04cb9282ad891bab653a3e8d7f97b14720996074
Congyue Cui <ccui@princeton.edu>
2019-05-10 19:36:57 -0400

    inversion

--- seispie/workflow/inversion.py
+++ seispie/workflow/inversion.py
@@ -1,31 +1,40 @@
 from seispie.workflow.base import base
+from time import time
+import numpy as np
 
-class inversion:
-	def setup(self, config):
-		if 'niter' in config:
-			self.niter = config['niter']
-		else:
-			self.niter = 1
-		
-		if 'slurm' in config:
-			self.slurm = config['slurm']
-		else:
-			self.slurm = 'no'
-		
-		if 'source_encoding' in config:
-			self.source_encoding = config['source_encoding']
-		else:
-			self.source_encoding = 'no'
-		
-		if 'save_gradient' in config:
-			self.save_gradient = config['save_gradient']
-		else:
-			self.save_gradient = 'no'
-		
-		if 'save_misfit' in config:
-			self.save_misfit = config['save_misfit']
-		else:
-			self.save_misfit = 'no'
+class inversion(base):
+	""" forward simulation
+	"""
+
+	def setup(self):
+		""" initialize
+		"""
+		pass
 	
-	def main(self):
-		print(self.save_gradient)
+	def run(self):
+		""" start workflow
+		"""
+		solver = self.solver
+		solver.import_model(1)
+		solver.import_sources()
+		solver.import_stations()
+
+		start = time()
+		solver.generate_traces()
+		mid = time()
+		print('')
+		print('Trace generation: %.2fs' % (mid - start))
+		print('')
+		solver.import_model(0)
+		solver.run_kernel(1)
+		print('')
+		print('Elapsed time: %.2fs' % (time() - mid))
+
+		out = np.zeros(solver.nx*solver.nz, dtype='float32')
+		solver.k_mu.copy_to_host(out, stream=solver.stream)
+		solver.stream.synchronize()
+		solver.export_field(out, 'kmu')
+
+	@property
+	def modules(self):
+		return ['solver', 'postprocess', 'optimize']
