f3e8441957805f88924c95bee0a1e43a320dd92a
Congyue Cui <ccui@princeton.edu>
2020-01-14 01:45:17 -0500

    update

--- seispie/optimize/base.py
+++ seispie/optimize/base.py
@@ -36,7 +36,6 @@ class base:
 					solver.export_field(self.m_new, 'mu', i-1)
 				else:
 					ref = misfit
-					print(ref)
 				misfits.append(misfit / ref)
 			
 			self.p_new = self.compute_direction()
