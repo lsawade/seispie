343b5979dcb86f6b6594fdfd8daa5ccf6292bd40
Congyue Cui <ccui@princeton.edu>
2020-01-13 22:18:57 -0500

    update

--- seispie/optimize/sgd.py
+++ seispie/optimize/sgd.py
@@ -1,4 +1,56 @@
 from seispie.optimize.gd import gd
 
 class sgd(gd):
-	pass
+	def run(self):
+		solver = self.solver
+		niter = int(self.config['niter'])
+		start = time()
+
+		misfits = []
+
+		head = 1
+		if self.mpi and self.mpi.rank():
+			head = 0
+
+		for i in range(niter):
+			if head:
+				print('Iteration %d' % (i+1))
+			
+			self.g_new, misfit, self.m_new = solver.compute_gradient()
+			
+			if head:
+				if i > 0:
+					solver.export_field(self.m_new, 'mu', i-1)
+				else:
+					ref = misfit
+				misfits.append(misfit / ref)
+			
+			self.p_new = self.compute_direction()
+			
+			self.m_old = self.m_new
+			self.p_old = self.p_new
+			self.g_old = self.g_new
+
+			if self.line_search(misfit) < 0:
+				if head:
+					print('Line search failed')
+					break
+
+			if head:
+				print('')
+
+		misfit = solver.compute_misfit()
+
+		if head:
+			misfits.append(misfit / ref)
+			m_new = np.zeros(solver.nx * solver.nz, dtype='float32')
+			solver.mu.copy_to_host(m_new, solver.stream)
+			solver.stream.synchronize()
+			solver.export_field(m_new, 'mu', niter)
+
+			print('Misfits:')
+			for misfit in misfits:
+				print('  %.4f' % misfit)
+
+			print('')
+			print('Elapsed time: %.2fs' % (time() - start))
